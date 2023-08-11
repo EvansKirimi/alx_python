9e0e2f34eabae28a7860175b6ada44f79298ef71
EvansKirimi <122809646+EvansKirimi@users.noreply.github.com>
2023-08-11 11:39:18 +0300

    finished task 3

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+new_number = str(number)
+last_number = new_number[-1:]
+last_digit = int(last_number)
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_number} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_number} and is 0")
+elif last_digit < 6 and last_number != 0:
+    print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
