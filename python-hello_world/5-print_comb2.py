50f8141074f8ecbb475f063687fde24c026bed7f
EvansKirimi <122809646+EvansKirimi@users.noreply.github.com>
2023-08-14 09:20:42 +0300

    Correction on task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print(f"{str(i)}{str(j)},")
+for i in range(0, 100):
+    if i == 99:
+        print(f"{i}")
+    else:
+        print("{:02}".format(i), end=", ")
