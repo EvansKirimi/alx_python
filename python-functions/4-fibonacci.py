2b6c01e25ee6423253126ee8d565fa4e26b6d37b
EvansKirimi <122809646+EvansKirimi@users.noreply.github.com>
2023-08-23 15:32:07 +0300

    correction

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -4,7 +4,7 @@ def fibonacci_sequence(n):
     if n <= 0:
         return []
     elif n == 1:
-        return [1]
+        return [0]
     elif n == 2:
         return [0, 1]
     sequence = [0, 1]
