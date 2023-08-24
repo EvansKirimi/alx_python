a1defcc0fe47cbefa3594058b337efdfcfaa6b2f
EvansKirimi <122809646+EvansKirimi@users.noreply.github.com>
2023-08-24 08:27:59 +0300

    Correction

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -2,8 +2,8 @@
 
 def is_prime(number):
     if number > 1:
-        for i in range(2, int(number/2)+1):
-            if (number % i) == 0:
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
                 return False
             else:
                 return True
