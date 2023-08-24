e3d28df49022a896c0a58503f6d1fd954d13467a
EvansKirimi <122809646+EvansKirimi@users.noreply.github.com>
2023-08-24 08:29:35 +0300

    correction

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def is_prime(number):
-    if number > 1:
-        for i in range(2, int(number ** 0.5) + 1):
-            if number % i == 0:
-                return False
-            else:
-                return True
-    else:
+    if number <= 1:
         return False
+
+    for divisor in range(2, int(number ** 0.5) + 1):
+        if number % divisor == 0:
+            return False
+
+    return True
